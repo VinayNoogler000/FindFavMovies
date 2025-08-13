93f2ecc6fb379413b1265b5f43f9d4f18039090c
VinayNoogler000 <vinaytambey000@gmail.com>
2025-08-13 19:48:35 +0530

    Add toggle functionality to faHeart icon on fav-btn click

--- src/components/FavButton.jsx
+++ src/components/FavButton.jsx
@@ -1,10 +1,18 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 
 export default function FavButton() {
+    const [isLiked, setIsLiked] = useState(false);
+
+    const toggleLikeStatus = () => {
+        setIsLiked( (likeStatus) => !likeStatus );
+    }
+
     return (
-        <button className="fav-btn" > 
-            <FontAwesomeIcon icon={faHeart} />
+        <button className="fav-btn" title="Add To Favorites" onClick={toggleLikeStatus} > 
+            <FontAwesomeIcon icon={ isLiked ? solidHeart : regularHeart } color="red" />
         </button>
     );
 }
