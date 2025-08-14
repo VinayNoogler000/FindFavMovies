e1558b0e86d0e77d5871737a58b815da78ff5a6b
VinayNoogler000 <vinaytambey000@gmail.com>
2025-08-14 18:06:24 +0530

    Add basic structure of Favorites Page

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+export default function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h3>No Favorite Movies Yet</h3>
+            <p>Start adding your favorite movies and they'll appear here!</p>
+        </div>
+    );
+}
