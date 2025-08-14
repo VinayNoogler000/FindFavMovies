c93758c585ede7a6e5c2a98b87c041f3868146a5
VinayNoogler000 <vinaytambey000@gmail.com>
2025-08-14 18:05:58 +0530

    Removed unnecessary form-attributes

--- src/components/SearchContent.jsx
+++ src/components/SearchContent.jsx
@@ -8,7 +8,7 @@ export default function SearchContent({ searchQuery, setSearchQuery }) {
     }
 
     return (
-        <form action="" className="content-search-form" onSubmit={handleSearch}>
+        <form className="content-search-form" onSubmit={handleSearch}>
             <label htmlFor="search-inp"> Search </label>
             <input type="text" id="search-inp" placeholder="Search for movies/shows..." name="content" value={searchQuery} onChange={ (e) => setSearchQuery(e.target.value) } required></input>
             <button type="submit" title="Search Movies/Shows" className="search-btn"> 
