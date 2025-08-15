88ac3a2dc77490bce3cb05779281b51897cebf8a
VinayNoogler000 <vinaytambey000@gmail.com>
2025-08-15 17:56:38 +0530

    Fix the path to the Home component import in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './css/App.css';
 import NavBar from "./components/NavBar";
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 
 function App() {
