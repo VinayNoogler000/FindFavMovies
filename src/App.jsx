cda3f7b85bb86af5a304724561369994c3b3dd1a
VinayNoogler000 <vinaytambey000@gmail.com>
2025-08-19 01:14:42 +0530

    Add feature to reset the state of Home page in a SPA-Friendly way by using `useLocation` hook from `react-router-dom`

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './css/App.css';
 import NavBar from "./components/NavBar";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { MovieProvider } from "./contexts/MovieContext";
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 
 function App() {
+  const location = useLocation();
+
   return (
     <MovieProvider>
       <header>
@@ -14,7 +16,7 @@ function App() {
 
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home key={location.key} />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
